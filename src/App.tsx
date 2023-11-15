8bb8c8c3d2a7f612a102eaf2776ac7bce83732cb
miszelo <michal.kawczak@gmail.com>
2023-11-15 13:38:08 +0100

    install styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { Header } from "./components/Header.tsx";
 import { Footer } from "./components/Footer.tsx";
 import { MyMap } from "./components/MyMap.tsx";
+import {MainContent} from "./styled/MainContent.ts";
 
 function App() {
   return (
-    <>
+    <MainContent>
       <Header />
       <MyMap />
       <Footer />
-    </>
+    </MainContent>
   );
 }
 
---
+++ src/styled/MainContent.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const MainContent = styled.div`
+  background-color: aqua;
+
+`;
