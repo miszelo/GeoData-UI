a84af80635605bc984865145066f5679cd200107
miszelo <michal.kawczak@gmail.com>
2023-11-19 22:57:17 +0100

    create apiCaller

---
+++ src/app/apiCaller.ts
@@ -0,0 +1,41 @@
+import { Register, useQuery, UseQueryResult } from "@tanstack/react-query";
+import { GeoData } from "./types/types";
+
+export const fetchData = (): UseQueryResult<
+  GeoData[],
+  Register extends { defaultError: infer TError } ? TError : Error
+> => {
+  return useQuery({
+    queryKey: ["currentData"],
+    queryFn: async () =>
+      await fetch("http://localhost:8080/api/v1/smog-data").then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      }),
+  });
+};
+
+export const fetchDataBy = (
+  key: "city" | "school",
+  value: string,
+  queryKey: string,
+  date: string = new Date().toISOString().slice(0, 10),
+): UseQueryResult<
+  GeoData[],
+  Register extends { defaultError: infer TError } ? TError : Error
+> => {
+  return useQuery({
+    queryKey: [queryKey],
+    queryFn: async () =>
+      await fetch(
+        `http://localhost:8080/api/v1/smog-data/${key}/${value}?date=${date}`,
+      ).then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      }),
+  });
+};
