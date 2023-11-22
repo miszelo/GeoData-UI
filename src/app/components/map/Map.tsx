6ccacfde3058a26d33ded2a44cc57b8353815fd4
miszelo <michal.kawczak@gmail.com>
2023-11-22 13:00:13 +0100

    style popup

--- src/app/components/map/Map.tsx
+++ src/app/components/map/Map.tsx
@@ -6,6 +6,7 @@ import { GeoData } from "../../types/types.ts";
 import "leaflet/dist/leaflet.css";
 import { getIcon } from "../../utils/mapUtils";
 import { formatDateToString } from "../../utils/stringUtils";
+import { StyledPopup } from "./MapStyles";
 
 type props = {
   isLoading: boolean;
@@ -19,47 +20,75 @@ export const Map: FC<props> = ({ isLoading, error, data }) => {
   if (error) return "error";
 
   return (
-    <MapContainer
-      className={"map"}
-      center={[52.112795, 19.211946]}
-      zoom={6}
-      maxZoom={20}
-      scrollWheelZoom={true}
-    >
-      <MarkerClusterGroup showCoverageOnHover={false}>
-        {data &&
-          data.map((data, index) => {
-            return (
-              <Marker
-                key={index}
-                position={{
-                  lat: data.place.coordinates.latitude,
-                  lng: data.place.coordinates.longitude,
-                }}
-                icon={getIcon(data)}
-              >
-                <Popup>
-                  <h3>{data.place.name}</h3>
-                  <h4>Aktualne dane: {formatDateToString(data.timestamp)}</h4>
-                  <p>
-                    PM10: {data.pm10.toPrecision(data.pm10 > 10 ? 4 : 3)} µg/m³
-                  </p>
-                  <p>
-                    PM2.5: {data.pm25.toPrecision(data.pm25 > 10 ? 4 : 3)} µg/m³
-                  </p>
-                  <p>Wilgotność: {data.humidity.toPrecision(4)} g/m³</p>
-                  <p>Ciśnienie: {data.pressure.toPrecision(5)} hPa</p>
-                  <p>Temperatura: {data.temperature.toPrecision(2)}°C</p>
-                </Popup>
-              </Marker>
-            );
-          })}
-      </MarkerClusterGroup>
-
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
+    <>
+      <MapContainer
+        className={"map"}
+        center={[52.112795, 19.211946]}
+        zoom={6}
+        maxZoom={20}
+        scrollWheelZoom={true}
+      >
+        <MarkerClusterGroup showCoverageOnHover={false}>
+          {data &&
+            data.map((data, index) => {
+              return (
+                <Marker
+                  key={index}
+                  position={{
+                    lat: data.place.coordinates.latitude,
+                    lng: data.place.coordinates.longitude,
+                  }}
+                  icon={getIcon(data)}
+                >
+                  <Popup>
+                    <StyledPopup>
+                      <h3>{data.place.name}</h3>
+                      <h4>Dane z {formatDateToString(data.timestamp)}</h4>
+                      <table>
+                        <tbody>
+                          <tr>
+                            <th>Parametr</th>
+                            <th>Wartość</th>
+                          </tr>
+                          <tr>
+                            <td>PM10</td>
+                            <td>
+                              {data.pm10.toPrecision(data.pm10 > 10 ? 4 : 3)}{" "}
+                              µg/m³
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>PM2.5</td>
+                            <td>
+                              {data.pm25.toPrecision(data.pm25 > 10 ? 4 : 3)}{" "}
+                              µg/m³
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>Wilgotność</td>
+                            <td>{data.humidity.toPrecision(4)} g/m³</td>
+                          </tr>
+                          <tr>
+                            <td>Ciśnienie</td>
+                            <td>{data.pressure.toPrecision(5)} hPa</td>
+                          </tr>
+                          <tr>
+                            <td>Temperatura</td>
+                            <td>{data.temperature.toPrecision(2)}°C</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </StyledPopup>
+                  </Popup>
+                </Marker>
+              );
+            })}
+        </MarkerClusterGroup>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </>
   );
 };
--- src/app/components/map/MapStyles.ts
+++ src/app/components/map/MapStyles.ts
@@ -10,3 +10,39 @@ export const MapContainer = styled.div`
 `;
 
 export const MapTitle = styled.h3``;
+
+export const StyledPopup = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+
+  h3 {
+    font-size: 1.2em;
+    margin-bottom: 8px;
+    color: #333;
+    word-break: break-word;
+  }
+
+  h4 {
+    font-size: 1em;
+    color: #888;
+    margin-bottom: 8px;
+  }
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 8px;
+
+    th,
+    td {
+      padding: 8px;
+      border: 1px solid #ddd;
+      text-align: left;
+    }
+
+    th {
+      background-color: #f2f2f2;
+    }
+  }
+`;
